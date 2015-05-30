486855b3f515ca25b2d5c8ca5d4f4acbe2c101ca
thefeeltrain <braydeneriq@gmail.com>
2015-05-30 12:29:22 -0700

    Multiple changes
    Custom games lobby reloads every time
    Server browser loads gametype and server name
    HTML cleaned up a bit
    Converted CSS background to shorthand
    Gametype icons moved to img/gametypes

--- js/dewrito.js
+++ js/dewrito.js
@@ -4,6 +4,13 @@
         https://creativecommons.org/licenses/by-nc-sa/4.0/
     */
 
+    var players = [],
+        joined = 0,
+        track = 5,
+        scale = 1,
+        anit = 400,
+        currentGame = "HaloOnline";
+
     function changeSetting(s,by) {
         $('#click')[0].currentTime = 0;
         $('#click')[0].play();
@@ -53,8 +60,6 @@
         $('#back').click(function() { changeMenu($(this).attr('data-action')); });
     });
 
-    var players = [], joined = 0, track = 5, scale = 1, anit = 400;
-
     function loadMaps() {
         var b,g,i,e;
         for(i=0; i < Object.keys(maps).length; i++) {
@@ -70,12 +75,27 @@
         return this.replace(/\w\S*/g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();});
     };
 
+    function acr(s){
+        var words, acronym, nextWord;
+        words = s.split(' ');
+        acronym= "";
+        index = 0;
+        while (index<words.length) {
+            nextWord = words[index];
+            acronym = acronym + nextWord.charAt(0);
+            index = index + 1;
+        }
+        return acronym.toUpperCase();
+    }
+
     function loadServers() {
         $('#browser').empty();
-        $.getJSON( "servers.json", function( data ) {
+        $.getJSON( "servers.json", function(data) {
             servers = data;
             for(var i=0; i<servers.length; i++) {
                 var p = (servers[i].map.toLowerCase()).toTitleCase();
+                console.log(servers[i].gametype);
+                if(servers[i].gametype.length > 12) {servers[i].gametype = acr(servers[i].gametype);}
                 $('#browser').append("<div class='server' id='server"+i+"' data-server="+i+"><div class='thumb'><img src='img/maps/"+servers[i].map+".png'></div><div class='info'><span class='name'>"+servers[i].name+"</span><span class='settings'>"+servers[i].gametype+" on "+p+"</span></div><div class='players'>"+servers[i].players.current+"/"+servers[i].players.max+"</div></div>");
                 $('#server'+i).css("display","none");
                 $('#server'+i).delay(Math.floor(Math.random()*1000)+anit).fadeIn(anit);
@@ -84,44 +104,39 @@
                 $('#click')[0].currentTime = 0;
                 $('#click')[0].play();
             });
-            $('.server').click(function() {
-                var id = $(this).attr('data-server'),d;
-                $.getJSON("servers.json", function(json){
-                    d = json[id].map;
-                    changeMenu("serverbrowser-custom",d);
-                });
-            });
+            $('.server').click(function() {changeMenu("serverbrowser-custom",$(this).attr('data-server'));});
         });
     }
 
     function changeMenu(menu,details) {
         var f;
         if(menu == "main-custom") {
+            joined = 0;
+            $('#lobby').empty();
+            $('#lobby').append("<tr class='top'><td colspan='2'>Current Lobby <span id='joined'>0</span>/16</td></tr>");
             $('#customgame').attr('data-from','main');
             $('#dewrito').css({"opacity":0, "top":"920px"});
             $('#back').fadeIn(anit);
             $('#back').attr('data-action','custom-main');
             $('#customgame').css({"top":"0px"});
             $('#main').css({"top":"720px"});
-            if(joined === 0) {
-                $.getJSON( "players.json", function( data ) {
-                    players = data;
-                    for(var i=0; i<players.length; i++) {
-                        $('#lobby').append("<tr id='player"+i+"' class='"+players[i].color+"'><td class='name'>"+players[i].name+"</td><td class='rank'><img src='ranks/"+players[i].rank+".png'</td></tr>");
-                        $('#player'+i).css("display","none");
-                        $('#player'+i).delay(Math.floor(Math.random()*15000)).fadeIn(anit,callback);
-                    }
-                    function callback() {joined++; $('#joined').text(joined);}
-                    $('#lobby tr').hover(function() {
-                        $('#click')[0].currentTime = 0;
-                        $('#click')[0].play();
-                    });
-                    $('#lobby tr').click(function() {
-                        var e = $(this).children('.name').text();
-                        changeMenu("custom-player",e);
-                    });
+            $.getJSON( "players.json", function( data ) {
+                players = data;
+                for(var i=0; i<players.length; i++) {
+                    $('#lobby').append("<tr id='player"+i+"' class='"+players[i].color+"'><td class='name'>"+players[i].name+"</td><td class='rank'><img src='ranks/"+players[i].rank+".png'</td></tr>");
+                    $('#player'+i).css("display","none");
+                    $('#player'+i).delay(Math.floor(Math.random()*10000)).fadeIn(anit,callback);
+                }
+                function callback() {joined++; $('#joined').text(joined);}
+                $('#lobby tr').hover(function() {
+                    $('#click')[0].currentTime = 0;
+                    $('#click')[0].play();
                 });
-            }
+                $('#lobby tr').click(function() {
+                    var e = $(this).children('.name').text();
+                    changeMenu("custom-player",e);
+                });
+            });
         }
         if(menu == "custom-main") {
             $('#dewrito').css({"opacity":0.95, "top":"240px","-webkit-transition-timing-function":"400ms","-webkit-transition-delay":"0ms"});
@@ -130,11 +145,18 @@
             $('#back').attr('data-action','main-main2');
         }
         if(menu == "serverbrowser-custom" && details) {
-            changeMap2(details);
-            $('#serverbrowser').css({"top":"720px"});
-            $('#customgame').css({"top":"0px"});
-            $('#back').attr('data-action','custom-serverbrowser');
-            $('#customgame').attr('data-from','serverbrowser');
+            var d;
+            $.getJSON("servers.json", function(json){
+                d = json[details];
+                changeMap2(d.map);
+                $('#subtitle').text(d.name);
+                $('#gametype-display').text(d.gametype);
+                $('#gametype-icon').css('background',"url('img/gametypes/"+d.gametype+".png') no-repeat 0 0/cover");
+                $('#serverbrowser').css({"top":"720px"});
+                $('#customgame').css({"top":"0px"});
+                $('#back').attr('data-action','custom-serverbrowser');
+                $('#customgame').attr('data-from','serverbrowser');
+            });
         }
         if(menu == "custom-serverbrowser") {
             $('#customgame').css({"top":"-720px"});
@@ -230,15 +252,13 @@
         $('#black').fadeIn(3500).delay(5000).fadeOut(1000, function() {$('#music')[0].play();});
     }
 
-    var currentGame = "HaloOnline";
-
     function changeMap1(game) {
         $('.map-select .selection').removeClass('selected');
         $("[data-game='"+game+"']").addClass('selected');
         $('.map-select').css({"left":"100px"});
         $('#maps-'+currentGame).hide().css({"left":"310px", "opacity":0});
         $('#maps-'+game).css('display', 'block');
-        $('#maps-'+game).animate({"left":"360px", "opacity":1},anit/4);
+        $('#maps-'+game).animate({"left":"360px", "opacity":1},anit/8);
         currentGame = game;
         $('#slide')[0].currentTime = 0;
         $('#slide')[0].play();
