35350000a38a31c535636c33bffbc31910f212ff
Shadowfita <www.ryan.palmer@hotmail.com>
2015-07-09 01:02:35 +1000

    Settings(LAST)

--- js/settings.js
+++ js/settings.js
@@ -46,7 +46,6 @@ settings = {
           }
           for (var i = 0; i < settings.musictrack.labels.length; i++) {
             if (settings.preset.labels[c] === "Halo 4") {
-              console.log('wat.');
               settings.musictrack.current = 10;
             } else if (settings.musictrack.labels[i] === settings.preset.labels[c]) {
               settings.musictrack.current = i;
