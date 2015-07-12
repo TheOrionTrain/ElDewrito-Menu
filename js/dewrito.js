e23fbd13b46a24d2f15e28b4b5c2bb4be26c96a0
Shadowfita <www.ryan.palmer@hotmail.com>
2015-07-12 11:37:35 +1000

    New Colours on Join

--- js/dewrito.js
+++ js/dewrito.js
@@ -603,7 +603,7 @@ function playersJoin(number, max, time, ip) {
 		for (var i = 0; i < serverInfo.numPlayers; i++) {
 			if (typeof players[i] != 'undefined' && players[i].name != "") {
 				if (teamGame)
-					colour = (parseInt(players[i].team) === 0) ? "#800000" : "#000080";
+					colour = (parseInt(players[i].team) === 0) ? "#cf3e3e" : "#375799";
 				$('#lobby').append("<tr id='player" + i + "' team='" + players[i].team + "' hex-colour= '" + colour + "' data-color='" + hexToRgb(colour, 0.5) + "' style='background:" + hexToRgb(colour, 0.5) + ";'><td class='name'>" + players[i].name + "</td><td class='rank'><img src='img/ranks/38.png'</td></tr>");
 				$('#player' + i).css("display", "none");
 				$('#player' + i).fadeIn(anit);
